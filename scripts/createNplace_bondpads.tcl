5f4669501968f8469451d4d6a0a58f359d611193
celuk <seyyid4091@gmail.com>
2024-12-07 01:50:00 +0300

    add license disclaimer to the scripts

--- scripts/createNplace_bondpads.tcl
+++ scripts/createNplace_bondpads.tcl
@@ -1,11 +1,19 @@
-#get_db insts .base_cell.name -u
-#get_db base_cell:$BONDPAD_CELL .class
-#get_db base_cells -if {.class == pad}
-#get_db base_cells -if {.class == block}
-#get_db insts .name -u
-#get_db insts -if {.base_cell.class == pad} -u
-#get_db [get_db insts -if {.base_cell.class == pad} -u] .name
-#get_db [get_db insts -if {.base_cell.class == block} -u] .name
+# This file is part of https://github.com/celuk/cadence-flow
+# Copyright (C) 2024  Seyyid Hikmet Celik
+# 					  seyyid4091@gmail.com
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 proc createNplace_bondpads {args} {
   
